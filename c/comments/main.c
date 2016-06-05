60629eb81512eeb7782a14f9ebd18f95903092e1
Nikolai <mail@nikno.de>
2016-06-05 18:32:50 +0100

    added comments example

---
+++ c/comments/main.c
@@ -0,0 +1,16 @@
+/* This is a comment and will be ignored by the compiler.*/
+
+/* Comments can be used to explain what happens and can
+have multiple lines.*/
+
+#include <stdio.h> /*This includes the standard input/output library header*/
+
+/* The main function is the entry point into the progrm. */
+/* The arguments argc and argv are used to handle command line arguments
+discussed later.*/
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n"); /* This prints 'Hello World!' (the \n ends the line) */
+    system("PAUSE");	/*This just makes life with the IDE easier. */
+    return 0; /* The main program should exit by returning 0. */
+}
