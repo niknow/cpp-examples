7fabf802265c811506180f10ead43fb1f51f323c
Nikolai <mail@nikno.de>
2016-06-06 23:00:18 +0100

    added strings

---
+++ c/strings/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>     /* Include this library for handling strings. */
+
+int main(int argc, char *argv[])
+{
+    char s1[10];    /* Strings are arrays of chars. */
+    char s2[3] = "ld" ;        /* Strings can be initialized too. */
+    char r[] = "Good morning";  /* Size can be set automatically on initialization. */
+    char s3[3] = "01";  /* Strings are null-terminated, so leave the last 
+                           character for the null termination.*/
+
+    printf("%s\n", r);  /* Print a string on console. */
+
+    strcpy(s1, "Hello Wor");  /* Copy strings into each other. */
+
+    printf("%s\n",s1);  /* Prints "Hello Wor". */
+
+    printf("%s\n", strcat(s1, s2));  /* strcat concatinates strings. */
+
+    printf("%i\n", strlen(s1));     /* strlen gives the length of the string (without terminating null-character) */
+
+    char *p = strstr(s1, "ello");  /* strstr returns a pointer to the first 
+                                      occurence of the second string in the first. */
+
+    system("PAUSE");
+    return 0;
+}
