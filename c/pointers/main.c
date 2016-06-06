482efd3ddddccd1645aec63006affec77607df22
Nikolai <mail@nikno.de>
2016-06-06 23:13:10 +0100

    added pointers

---
+++ c/pointers/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* A pointer is simply a variable that stores the memory address 
+of another variable. */
+
+int main(int argc, char *argv[])
+{
+    int x=5, y; /* These are some integer variables. */
+    int *p;     /* This is a pointer to an integer variable. */
+    int **a;    /* This is a pointer to a pointer to an integer variable. */
+
+    p = &x;     /* Now p contains the memory address of the integer x. */
+                /* We say 'p points to x'. */
+
+    printf("p: %p\n",p);    /* Gives something like 0028FF44.*/
+
+    y = *p;   /* Now y contains the content of the variable to which p points 
+                 to, i.e. to the variable with address p (x in this case). */
+
+    printf("y: %i\n",y);    /* Yields 5, because x was initialized to 5. */
+
+    /* You can play the same game with pointers of pointers. */
+
+    a = &p; /* Now a contains the memory address of the pointer p. */
+
+    printf("a: %p\n",a); /* Gives something like 0028FF3C. */
+    printf("*a: %i\n", *a); /* Gets the pointer, something like 0028FF44. */
+    printf("**a: %i\n", **a); /* Gets the content, in this case 5.*/
+
+    system("PAUSE");
+    return 0;
+
+}
