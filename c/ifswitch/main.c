82e01a44c1c95f2f2eaf7acc65bda8f9b1ee28cc
Nikolai Nowaczyk <mail@nikno.de>
2021-04-25 20:35:30 +0100

    formatting and tweaks

--- c/ifswitch/main.c
+++ c/ifswitch/main.c
@@ -7,46 +7,56 @@ operator, the 'if'-keyword and the 'switch'-keyword. */
 
 int main(int argc, char* argv[])
 {
-	int x = 0, y = 1;
+    int x = 0, y = 1;
 
-	/*the terniary ? operator*/
-	x > 0 ? printf("yes\n") : printf("no\n"); /* prints 'yes' */
+    /* the terniary ? operator */
+    x > 0 ? printf("?: yes\n") : printf("?: no\n");   /* prints 'yes' */
 
-	/*if keyword */
-	if (x == 1) printf("x: Yeah\n");    /* will not be printed */
-	if (y == 1) printf("y: Yeah\n");    /* will be printed */
-	if (y == 1) {
-		printf("y: Foo\n"); /* If more than 1 line of code should be executed, */
-		printf("y: Bar\n"); /* we need to put them in a block. */
-	}
+    /* if keyword: is used to execute code only if a condition is true */
+    if (x == 1)
+        printf("x: Yeah\n");    /* will not be printed */
 
-	if (x >= 0 & y != 0) {     /* more complex boolean expressions are possible */
-		printf("Yeah Yeah\n");   /* It is also allowed to put 1 line into a block. */
-	}
-	else if (x >= 0) {
-		printf("Yeah\n");   /* This is not executed because the first 'if' matched.*/
-		if (y > 0) printf("ah"); /* if's can be nested */
-	}
-	else {
-		printf("Boo.\n"); /* Not executed because first 'if' matched.*/
-	}
+    if (y == 1)
+        printf("y: Yeah\n");    /* will be printed */
 
-	if (x < 0)
-		printf("x < 0 \n");  /* Not executed. */
-	else
-		printf("x >= 0 \n"); /* Executed because first 'if' didn't match.*/
+    if (y == 1) {
+        printf("y: Foo\n");     /* If more than 1 line of code should be executed, */
+        printf("y: Bar\n");     /* we need to put them in a block. */
+    }
 
-	/* switch keyword */
-	switch (x) {
-	case 0:
-		printf("x==0\n");
-		break; /* optional, but otherwise the next block will be executed! */
-	case 1:
-		printf("x==1\n");
-	default:   /* this is optional and is executed if no other match is found */
-		printf("something else");
-	}
+    if ( (x >= 0) && (y != 0) ) {     /* more complex boolean expressions are possible */
+        printf("Yeah Yeah\n");        /* it is also allowed to put 1 line into a block. */
+    }
+    else if (x >= 0) {                /* checked only if first 'if' doesn't match*/
+        printf("Yeah\n");
+        if (y > 0)                    /* if's can be nested */
+            printf("ah");     
+    }
+    else {                            /* only executed if none of the previous conditions matched */          
+        printf("Boo.\n"); 
+    }
+    /* for boolean expressions keep in mind: 
+      &&: and
+      ||: or
+      ! : not
+    */
 
-	system("PAUSE");
-	return 0;
+    if (x < 0)
+        printf("x < 0 \n");     /* not executed. */
+    else
+        printf("x >= 0 \n");    /* cxecuted because first 'if' didn't match.*/
+
+    /* switch keyword: used to distinguish a fixed set of cases (alternative to if) */
+    switch (x) {
+    case 0:
+        printf("x==0\n");
+        break; /* optional, but otherwise the next block will be executed! */
+               /* this is because 'switch' works more like a goto to the 'case' label */
+    case 1:
+        printf("x==1\n");
+    default:   /* this is optional and is executed if no other match is found */
+        printf("something else");
+    }
+
+    return 0;
 }
--- c/loops/main.c
+++ c/loops/main.c
@@ -3,42 +3,42 @@
 
 /* Loops are a mean to execute the same code over and over again. There are
 three types of loops in C: The 'for'-loop, the 'while'-loop and the
-'do'-'while'*-loop. */
+'do..while'-loop. */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    int i, n=5;
+    int i, n = 5;
 
-    /* 'for'-loops: */
-    for(i=0;            i<n;       i++) /*prints the first 5 numbers*/
-    /*  initialization; condition; increment*/
-        printf("%i\n",i);   /* statement */
+    /* 'for'-loops: number of iterations is known in advance*/
+    for (i = 0; i < n; i++)      /* prints the first 5 numbers*/
+    /*  1. initialization; 2. condition; 3. increment*/
+        printf("for: i: %i\n", i);       /* 4. statement */
 
-    for(i=0; i<n & n!=2; i+=2){ /*all four parts can be more complex*/
-        printf("%i\n",i);
+    for (i = 0; (i < n) && (n != 2); i += 2) { /*all four parts can be more complex*/
+        printf("for2: i: %i\n", i);
+        printf("for2: i**2: %i\n", i*i);
     }
 
-    for(i=0; i<n; i++){
-        if(i==1)
-            continue; /* skips the rest of the loop for i=1*/
-        if(i==3)
-            break;  /* exits the loop at i=3 */
+    for (i = 0; i < n; i++) {
+        if (i == 1)
+            continue;  /* skips the rest of the loop for i=1*/
+        if (i == 3)
+            break;     /* exits the loop at i=3 */
     }
 
-    /* while: entry condition */
-    i=0;
-    while(i<n){ /* executed till as long as entry condition holds (might stop before first iteration) */
-        printf("%i",i);
+    /* while: loop with entry condition */
+    i = 0;
+    while (i < n) { /* executed as long as entry condition holds (might stop before first iteration) */
+        printf("while: %i\n", i);
         i++;
     }
 
-    /*do while: exit condition  */
-    i=0;
-    do{ /* Start of the loop. At least one iteration is executed. */
-        printf("%i",i);
+    /*do while: loop with exit condition  */
+    i = 0;
+    do { /* start of the loop. At least one iteration is executed. */
+        printf("do..while: %i\n", i);
         i++;
-    } while (i<n);  /* Iteration stops of exit condition is met. */
+    } while (i < n);  /* iteration stops if exit condition is met. */
 
-    system("PAUSE");
     return 0;
 }
