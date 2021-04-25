b384465041e79099e7f1ac8c9f95af1effe95757
Nikolai Nowaczyk <mail@nikno.de>
2021-04-25 20:14:35 +0100

    reformatting and tweaking

--- c/comments/main.c
+++ c/comments/main.c
@@ -2,15 +2,15 @@
 
 /* Comments can be used to explain what happens and can
 have multiple lines.*/
+/* (C++ distinguishes between single and multi line comments.) */
 
 #include <stdio.h> /*This includes the standard input/output library header*/
 
 /* The main function is the entry point into the progrm. */
 /* The arguments argc and argv are used to handle command line arguments
 discussed later.*/
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     printf("Hello World!\n"); /* This prints 'Hello World!' (the \n ends the line) */
-    system("PAUSE");	/*This just makes life with the IDE easier. */
     return 0; /* The main program should exit by returning 0. */
 }
--- c/variables/main.c
+++ c/variables/main.c
@@ -5,44 +5,46 @@
 variable before its first use and state its type. In C variables should
 be declared at the beginning of their scope. */
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-
     /* the basic datatypes: */
     int a;         /* integer */
     float b;       /* floating point */
     double c;      /* double precision floating point*/
     char d;        /* character */
 
-    int x = 5;  /*variables can be initialized on declaration*/
-    int y, z;  /*these are several integers*/
+    int x = 5;     /* variables can be initialized on declaration*/
+    int y, z;      /* these are several integers*/
 
     /* datatypes can be modified */
-    signed int e;  /* positive and negative integers (=integers) */
-    unsigned int f; /* only non-negative integers but larger range*/
+    signed int e;    /* positive and negative integers (=integers) */
+    unsigned int f;  /* only non-negative integers but larger range */
     long int g;      /* larger range */
     short int h;     /* shorter range, less memory */
+    /* full ist: https://en.wikipedia.org/wiki/C_data_types*/
 
     /* you can assign values to unitizialized and initialized variables later */
     x = 5;
     y = 6;
-    z = x+y; /* =11 */
-    z = x*y; /* =30 */
-    z = x-y; /* =-1 */
-    z = x/y; /* =0,  because integer division is truncated*/
-    b = (float) x/y;    /* = 0.83333, because we made a typecast*/
+    b = 7.3f;            /* the 'f' stands for 'float'  */
+    c = 7.3;             /* without f the number is treated as double*/
+    z = x + y;           /* == 11 */
+    z = x * y;           /* == 30 */
+    z = x - y;           /* == -1 */
+    z = x / y;           /* == 0,  because integer division is truncated */
+    b = (float)x / y;    /* == 0.83333, because we made a typecast */
 
-    x += 5; /*equivalent to x = x + 5*; same for - * or / */
-    x++; /* equivalent to x = x+1; same for - */
+    x += 5;    /* equivalent to x = x + 5*; same for - * or / */
+    x++;       /* equivalent to x = x+1; same for -; expression evaluates to x */
+    ++x;       /* same as x++, but expression evaluates to x + 1 */
 
-    b = 5.0/6.0; /* gives 0.8333...*/
-    b = 0.78;    /* use dot to denote the comma */
-    b = 0.78e7;  /* you can just use scientific notation*/
+    c = 5.0 / 6.0;    /* gives 0.8333... */
+    c = 0.78;         /* use dot to denote the comma */
+    c = 0.78e7;       /* you can just use scientific notation */
 
-    /* printing is a topic in its own*/
+    /* printing is a topic in its own */
     printf("x: %i\n", x);
     printf("b: %f\n", b);
 
-    system("PAUSE");
     return 0;
 }
